6d1924055569a7a65b00539d0c3937d50848f6e8
AlexMeow <oo587868@hotmail.com>
2024-07-01 19:27:00 +0800

    cross origin加上內網IP

--- src/main/java/com/example/artisan/config/WebConfig.java
+++ src/main/java/com/example/artisan/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://192.168.100.179:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true)
